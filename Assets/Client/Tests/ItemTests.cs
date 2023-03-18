615835d87f71a28e503b662c5b295c138aaa4e44
Mishxa-P <mishxa.petrov.01@gmail.com>
2023-03-18 06:51:28 +0300

    Update Item Tests

--- Assets/Client/Tests/ItemTests.cs
+++ Assets/Client/Tests/ItemTests.cs
@@ -7,40 +7,44 @@ using UnityEngine.TestTools;
 public class ItemTests
 {
     private GameObject utils;
-    private GameObject shop;
 
     [SetUp]
     public void Setup()
     {
         utils = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Utils.prefab");
-        utils = GameObject.Instantiate(utils);
-        shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
-        shop = GameObject.Instantiate(shop);
+        utils = GameObject.Instantiate(utils);  
     }
 
     [TearDown]
     public void TearDown()
     {
         Object.Destroy(utils);
-        Object.Destroy(shop);
     }
 
     [Test]
     public void ShopGetBuff()
     {
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
         Assert.AreNotEqual(string.IsNullOrEmpty(""), string.IsNullOrEmpty(shop.GetComponent<Shop>().GetBuff().GetBuffDescription()));
+        Object.Destroy(shop);
     }
 
     [Test]
     public void ShopGetWeapon()
     {
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
         Assert.AreNotEqual(string.IsNullOrEmpty(""), string.IsNullOrEmpty(shop.GetComponent<Shop>().GetWeapon().GetWeaponDescription()));
+        Object.Destroy(shop);
     }
 
 
     [UnityTest]
     public IEnumerator ShopCollisionWithEnemyPatrol()
     {
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
         GameObject enemy =
             AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/EnemyPatrol.prefab");
         enemy = GameObject.Instantiate(enemy);
@@ -52,6 +56,7 @@ public class ItemTests
         Assert.AreEqual(shop.GetComponent<Shop>().getEnemiesNearby(), true);
 
         Object.Destroy(enemy);
+        Object.Destroy(shop);
     }
 
     [UnityTest]
@@ -59,6 +64,8 @@ public class ItemTests
     {
         GameObject player = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Tests/TestPlayer.prefab");
         player = GameObject.Instantiate(player);
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
         GameObject enemy =
            AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/EnemyPathFinder.prefab");
         enemy = GameObject.Instantiate(enemy);
@@ -70,12 +77,15 @@ public class ItemTests
         Assert.AreEqual(shop.GetComponent<Shop>().getEnemiesNearby(), true);
 
         Object.Destroy(enemy);
+        Object.Destroy(shop);
         Object.Destroy(player);
     }
 
     [UnityTest]
     public IEnumerator ShopCollisionWithEnemyPatrolZone()
     {
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
         GameObject enemy =
             AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/EnemyPatrolZone.prefab");
         enemy = GameObject.Instantiate(enemy);
@@ -87,6 +97,49 @@ public class ItemTests
         Assert.AreEqual(shop.GetComponent<Shop>().getEnemiesNearby(), true);
 
         Object.Destroy(enemy);
+        Object.Destroy(shop);
     }
+
+    [UnityTest]
+    public IEnumerator PlayerBuyInShop()
+    {
+        GameObject platform = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Platform1.prefab");
+        platform = GameObject.Instantiate(platform);
+        GameObject player = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Tests/TestPlayer.prefab");
+        player = GameObject.Instantiate(player);
+        GameObject shop = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Client/Prefabs/Shop.prefab");
+        shop = GameObject.Instantiate(shop);
+
+        DataHolder.localCoins = new Coin(1000);
+        platform.transform.position = player.transform.position - new Vector3(0.0f, 1.0f, 0.0f);
+
+        yield return new WaitForSeconds(0.1f);
+        shop.transform.position = player.transform.position;
+        shop.transform.position += new Vector3(0.667f, 0.0f, 0.0f);
+
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<PlayerController>().PickUpItem();
+        shop.transform.position -= new Vector3(0.667f, 0.0f, 0.0f);
+
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<PlayerController>().PickUpItem();
+        shop.transform.position -= new Vector3(1.431f, 0.0f, 0.0f);
+
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<PlayerController>().PickUpItem();
+        shop.transform.position -= new Vector3(1.09f, 0.0f, 0.0f);
+
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<PlayerController>().PickUpItem();
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(shop.transform.childCount, 0);
+     
+        DataHolder.localCoins = new Coin(0);
+        Object.Destroy(player);
+        Object.Destroy(platform);
+        Object.Destroy(shop);
+    }  
 
 }
